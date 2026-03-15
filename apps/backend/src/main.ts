74de6fb348dff887ac476e6d2f2252c8cbadebf5
meleadr <meledina.adriano@gmail.com>
2026-03-15 17:36:33 +0100

    feat: integrate Prisma with PostgreSQL adapter and update environment configuration

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -12,6 +12,9 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  app.enableCors({
+    port: 4200,
+  });
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
